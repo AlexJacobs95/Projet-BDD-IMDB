b7dd83277ee90d0f51a52b7b0477281add3307a8
Alexandre Jacobs <alejacob@ulb.ac.be>
2017-04-10 19:55:14 +0200

    make some changes

--- download_file_unzip.sh
+++ download_file_unzip.sh
@@ -1,18 +1,9 @@
+#!/bin/bash
 # Script pour ddl les fichiers depuis le site et les unzip directement 
 # et supprime aussi les fichier non nécessaire.
+rm -f *.list
 FILETODELETE="aka-names.list.gz aka-titles.list.gz alternate-versions.list.gz biographies.list.gz business.list.gz certificates.list.gz color-info.list.gz costume-designers.list.gz crazy-credits.list.gz german-aka-titles.list.gz goofs.list.gz iso-aka-titles.list.gz italians-aka-titles.list.gz keywords.list.gz laserdisc.list.gz literature.list.gz miscellaneous-companies.list.gz miscellaneous.list.gz special-effects-companies.list.gz taglines.list.gz technical.list.gz trivia.list.gz"
 
 wget -r --accept="*.gz" --no-directories --no-host-directories --level 1 ftp://ftp.fu-berlin.de/pub/misc/movies/database/
 
-#supprime les fichiers non utile.
 rm -f $FILETODELETE
-
-#unzip des fichiers
-gzip -d *.gz
-
-echo 'affiche les fichier unzip'
-ls *.list
-
-echo 'voir si pas de gz'
-ls *gz 
-echo "tout est ok !"
