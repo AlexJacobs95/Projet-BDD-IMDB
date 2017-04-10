1e3d0837485d8953da12b68e0db46ba9abd586d9
Alexandre Jacobs <alejacob@ulb.ac.be>
2017-04-10 20:55:11 +0200

    begin implement website

---
+++ site_web/welcome_page.php
@@ -0,0 +1,24 @@
+<!--
+Jacobs Alexandre, Engelman David, Engelman Benjamin.
+INFO-H-303 : Bases de données - Projet IMBD.
+Page d'acceuill de site web.
+-->
+
+ <?php session_start(); ?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<link href="media/icon_imdb.ico" rel="shortcut icon" type="image/x-icon" />
+		<title>IMBD Welcome</title>
+		<meta charset="utf-8" />
+	</head>
+	<body>
+		<div class="col-sm-5 col-sm-offset-2 col-md-10 col-md-offset-2 main">
+				<div class="page-header">
+					<h1>IMDB: Internet Movie Data Base</h1> 
+					<p>Bienvenue sur le moteur de recherche cinématographique!</p> 
+				</div>
+		</div>
+	</body>
+</html>
