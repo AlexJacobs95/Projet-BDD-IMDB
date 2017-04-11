90310859cf6e587a961dd278db02db427f68660d
David <David.Engelman@ulb.ac.be>
2017-04-11 17:25:43 +0200

    welcome_page

--- site_web/menubar.php
+++ site_web/menubar.php
@@ -3,14 +3,11 @@
 	- recherche avancé
 	- connexion administrateur
 -->
-<div class=header>
-	<div class=navcontainer>
-            <ul class="nav navbar-nav">
-                <li><a href="welcome_page.php">Accueil</a></li>
-                <li><a href="simple_search.php">Recherche simple</a></li>
-                <li><a href="advanced_search.php">Recherche avancée</a></li>
-                <li><a href="administrator_page.php">Administrateur</a></li>
-            </ul>
-    </div>
-</div>
+<ul class="nav navbar-nav">
+    <li><a href="welcome_page.php">Accueil</a></li>
+    <li><a href="simple_search.php">Recherche simple</a></li>
+    <li><a href="advanced_search.php">Recherche avancée</a></li>
+    <li><a href="administrator_page.php">Administrateur</a></li>
+</ul>
+
 
--- site_web/welcome_page.php
+++ site_web/welcome_page.php
@@ -7,26 +7,133 @@ Page d'acceuill de site web.
  <?php session_start(); ?>
 
 <!DOCTYPE html>
-<html>
-	<link href="./css/bootstrap.min.css" rel="stylesheet">
-	<link href="./css/dashboard.css" rel="stylesheet">
-	<link rel="stylesheet" href="./css/style.css" type="text/css">
-	<head>
-		<link href="media/icon_imdb.ico" rel="shortcut icon"/>
-		<title>IMBD Welcome</title>
-		<meta charset="utf-8" />
-	</head>
-	<body>
-		<?php 
-			include 'menubar.php';
-		?>
-		<div class="billboard">
-			<div class="text-wrapper">
-				<h1>IMDB: Internet Movie Data Base</h1> 
-				<p>Bienvenue sur le moteur de recherche cinématographique!</p> 
-			</div>
-		</div>
-	</body>
-	<script src="./js/jquery-1.12.3.min.js"></script>
-	<script src="./js/bootstrap.min.js"></script>
+<html lang="fr">
+
+<head>
+
+    <meta charset="utf-8">
+    <title>IMD - International movie database</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+    <!-- Theme CSS -->
+    <link href="test_css/agency.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js" integrity="sha384-0s5Pv64cNZJieYFkXYOTId2HMA2Lfb6q2nAcx2n0RTLUnCAoTTsS0nKEO27XyKcY" crossorigin="anonymous"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js" integrity="sha384-ZoaMbDF+4LeFxg6WdScQ9nnR1QC2MIRxA1O9KWEXQwns1G8UNyIEZIQidzb0T1fo" crossorigin="anonymous"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top" class="index">
+
+<!-- Navigation -->
+<nav id="mainNav" class="navbar navbar-default navbar-custom navbar-fixed-top">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header page-scroll">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+            </button>
+            <a class="navbar-brand page-scroll" href="#page-top">IMDB</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li class="hidden">
+                    <a href="#page-top"></a>
+                </li>
+                <li>
+                    <a href="href=simple_search.php">Recherche avancée</a>
+                </li>
+                <li>
+                    <a href="administrator_page.php">Administrateur</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container-fluid -->
+</nav>
+
+<!-- Header -->
+<header>
+    <div class="container">
+        <div class="intro-text">
+            <div class="intro-lead-in">Bienvenue sur IMDB!</div>
+            <div class="intro-heading">Recherchez un film, une série, un acteur et plus</div>
+
+            <!-- search bar -->
+            <form>
+                <input type="text" name="search" placeholder="Tapez ici votre recherche ...">
+            </form>
+
+        </div>
+    </div>
+
+
+</header>
+
+<!-- Stats Section -->
+<section id="stats" class="bg-light-gray">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2 class="section-heading">Sur ce site, sont référencés</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-4">
+                <div class="stat-member">
+                    <h3>3020394</h3>
+                    <h4>Filmes</h4>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="stat-member">
+                    <h3>234987</h3>
+                    <h4>Séries</h4>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="stat-member">
+                    <h3>6523412</h3>
+                    <h4>Acteurs</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- jQuery -->
+<script src="vendor/jquery/jquery.min.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+<!-- Plugin JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js" integrity="sha384-mE6eXfrb8jxl0rzJDBRanYqgBxtJ6Unn4/1F7q4xRRyIw7Vdg9jP4ycT7x1iVsgb" crossorigin="anonymous"></script>
+
+<!-- Contact Form JavaScript -->
+<script src="js_test/jqBootstrapValidation.js"></script>
+<script src="js_test/contact_me.js"></script>
+
+<!-- Theme JavaScript -->
+<script src="js_test/agency.min.js"></script>
+
+</body>
+
 </html>
