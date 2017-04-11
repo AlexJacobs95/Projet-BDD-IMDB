38a3c2bdbde47c282ce729c30b6e98b0aeb05529
Alexandre Jacobs <alejacob@ulb.ac.be>
2017-04-11 11:17:37 +0200

    add the menubar for basic action

---
+++ site_web/menubar.php
@@ -0,0 +1,15 @@
+<!-- Menu latéral avec différentes possibilité: 
+	- recherche simple
+	- recherche avancé
+	- connexion administrateur
+-->
+<div class=header>
+	<div class=navcontainer>
+            <ul class="nav navbar-nav">
+                <li><a href="simple_search.php">Recherche simple</a></li>
+                <li><a href="advanced_search.php">Recherche avancée</a></li>
+                <li><a href="administrator_page.php">Administrateur</a></li>
+            </ul>
+    </div>
+</div>
+
