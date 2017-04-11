7f9d858c4d4f60f586af0c57beff6837b739dd74
Alexandre Jacobs <alejacob@ulb.ac.be>
2017-04-11 11:16:05 +0200

    update with basic action and make a design

--- site_web/welcome_page.php
+++ site_web/welcome_page.php
@@ -8,17 +8,24 @@ Page d'acceuill de site web.
 
 <!DOCTYPE html>
 <html>
+	<!-- Bootstrap CSS core -->
+	<link href="./css/bootstrap.min.css" rel="stylesheet">
+	<link href="./css/dashboard.css" rel="stylesheet">
+	<link rel="stylesheet" href="./css/style.css" type="text/css">
 	<head>
-		<link href="media/icon_imdb.ico" rel="shortcut icon" type="image/x-icon" />
+		<link href="media/icon_imdb.ico" rel="shortcut icon"/>
 		<title>IMBD Welcome</title>
 		<meta charset="utf-8" />
 	</head>
 	<body>
-		<div class="col-sm-5 col-sm-offset-2 col-md-10 col-md-offset-2 main">
-				<div class="page-header">
-					<h1>IMDB: Internet Movie Data Base</h1> 
-					<p>Bienvenue sur le moteur de recherche cinématographique!</p> 
-				</div>
+		<?php 
+			include 'menubar.php';
+		?>
+		<div class="billboard">
+			<div class="text-wrapper">
+				<h1>IMDB: Internet Movie Data Base</h1> 
+				<p>Bienvenue sur le moteur de recherche cinématographique!</p> 
+			</div>
 		</div>
 	</body>
 </html>
