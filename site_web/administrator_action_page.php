b092530f79ff64129708640c76a2a3243b17cb09
Alexandre Jacobs <alejacob@ulb.ac.be>
2017-04-12 12:21:37 +0200

    add panel action admin

---
+++ site_web/administrator_action_page.php
@@ -0,0 +1,184 @@
+<!--
+Jacobs Alexandre, Engelman David, Engelman Benjamin.
+INFO-H-303 : Bases de données - Projet IMBD.
+Page de recherche avancé de site web.
+-->
+ <?php session_start(); ?>
+
+ <!DOCTYPE html>
+<html>
+	<head>
+
+    <meta charset="utf-8">
+    <title>IMD - International movie database</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+    <!-- Theme CSS -->
+    <link href="test_css/agency.css" rel="stylesheet">
+    
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js" integrity="sha384-0s5Pv64cNZJieYFkXYOTId2HMA2Lfb6q2nAcx2n0RTLUnCAoTTsS0nKEO27XyKcY" crossorigin="anonymous"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js" integrity="sha384-ZoaMbDF+4LeFxg6WdScQ9nnR1QC2MIRxA1O9KWEXQwns1G8UNyIEZIQidzb0T1fo" crossorigin="anonymous"></script>
+    <![endif]-->
+
+</head>
+	<body id="page-top" class="index">
+		<?php
+			include 'menubar.php';
+		?>
+		<header>
+    		<div class="container">
+                <div class="intro-text">
+                    <h1>Panneau Admnistrateur</h1>
+                    <style type="text/css">
+                        input[type=text], input[type=email], input[type=password] {
+                            width: 100%;
+                            padding:0;
+                            margin: 0;
+                            color: black;
+                            display: inline;
+                            border: 2.5px solid yellow;
+                            box-sizing: border-box;
+                            border-radius: 4px;
+                        }
+
+                        button {
+                            margin: 5px 0;
+                            background-color: #FFD700;
+                            color: black;
+                            border: none;
+                            cursor: default;
+                            border-radius: 4px;
+                        }
+
+                        button:hover {
+                            opacity: 0.5;
+                        }
+                    </style>
+                    <!-- Ajout Supp admin -->
+                    <form action="/action_administrator.php">
+                        <h4>Ajout Admnistrateur</h4>
+                        <label><b>Email</b></label>
+                        <input type="email" name="email" placeholder="Enter a email" required>
+                        <label><b>Password</b></label>
+                        <input type="password" name="pswd" placeholder="Enter a password" required>
+                        <button type="submit">Ajout</button>
+                    </form>
+                    <form action="/action_administrator">
+                        <h4>Suppression Admnistrateur</h4>
+                        <label><b>Email</b></label>
+                        <input type="email" name="email" placeholder="Enter a email" required>
+                        <button type="submit">Suppression</button>
+                    </form>
+                    
+                    <!-- ============================================== -->
+
+                    <!-- Opérations sur la base de donnée -->
+
+                    <form action="/action_administrator.php"></form>
+                        <h4>Ajout Film</h4>
+                        <input type="text" name="film_reference" placeholder="Enter the reference" required>
+                        <input type="text" name="film_name" placeholder="Enter the name" required>
+                        <input type="text" name="genre_film" placeholder="Enter the genre" required> 
+                        <input type="text" name="year_film" placeholder="Enter the year(optional)">
+                        <input type="text" name="rating_note_film" placeholder="Enter a rating note (optional)">
+                        <button type="submit">Ajout</button>
+                    </form>
+
+                    <form action="/action_administrator.php"></form>
+                        <h4>Suppression Film</h4>
+                        <input type="text" name="film_reference" placeholder="Enter the reference" required>
+                        <input type="text" name="film_name" placeholder="Enter the name" required>
+                        <button type="submit">Suppression</button>
+                    </form>
+
+                    <form action="/action_administrator.php"></form>
+                        <h4>Ajout Série</h4>
+                        <input type="text" name="serie_reference" placeholder="Enter the reference" required>
+                        <input type="text" name="serie_name" placeholder="Enter the name" required>
+                        <input type="text" name="genre_serie" placeholder="Enter the genre" required>
+                        <input type="text" name="begin_year" placeholder="Enter the begin year" required>
+                        <input type="text" name="end_year" placeholder="Enter the end year (optional)">
+                        <input type="text" name="rating_note_serie" placeholder="Enter a rating note(optional)">
+                        <button type="submit">Ajout</button>
+                    </form>
+
+
+                    <form action="/action_administrator.php"></form>
+                        <h4>Suppression Série</h4>
+                        <input type="text" name="serie_reference" placeholder="Enter the reference" required>
+                        <input type="text" name="serie_name" placeholder="Enter the name" required>
+                        <button type="submit">Suppression</button>
+                    </form>
+
+                    <form action="/action_administrator.php">
+                        <h4>Ajout Episode</h4>
+                        <input type="text" name="serie_name" placeholder="Enter the name of the serie" required>
+                        <input type="text" name="episode_name" placeholder="Enter the episode name" required>
+                        <input type="text" name="number_season" placeholder="Enter the number of the season" required>
+                        <input type="text" name="episode_number" placeholder="Enter the episode number" required>
+                        <input type="text" name="rating_note_episode" placeholder="Enter a rating note (optional)">
+                        <button type="submit">Ajout</button>
+                    </form>
+
+                    <form action="/action_administrator.php">
+                        <h4>Ajout Directeur</h4>
+                        <input type="text" name="director_firstname" placeholder="Enter the FirstName" required>
+                        <input type="text" name="director_secondname" placeholder="Enter the SecondName" required>
+                        <input type="text" name="gender" placeholder= "Enter the gender (optional if person already exist)">
+                        <button type="submit">Ajout</button>
+                    </form>
+
+                    <form action="/action_administrator.php">
+                        <h4>Ajout Ecrivain</h4>
+                        <input type="text" name="writter_firstname" placeholder="Enter the FirstName" required>
+                        <input type="text" name="writter_secondname" placeholder="Enter the SecondName" required>
+                        <input type="text" name="gender" placeholder= "Enter the gender (optional if person already exist)">
+                        <button type="submit">Ajout</button>
+                    </form>
+
+                    <form action="/action_administrator.php">
+                        <h4>Ajout Acteur/Actrice</h4>
+                        <input type="text" name="actor_firstname" placeholder="Enter the FirstName" required>
+                        <input type="text" name="actor_secondname" placeholder="Enter the SecondName" required>
+                        <input type="text" name="gender" placeholder= "Enter the gender (optional if person already exist)">
+                        <button type="submit">Ajout</button>
+                    </form>
+
+                    <form action="/action_administrator.php">
+                        <h4>Ajout Genre</h4>
+                        <input type="text" name="genre" placeholder="Enter a genre" required>
+                        <button type="submit">Ajout</button>
+                    </form>
+
+                    <form action="/action_administrator.php">
+                        <h4>Ajout Pays</h4>
+                        <input type="text" name="country" placeholder="Enter a country" required>
+                        <button type="submit">Ajout</button>
+                    </form>
+
+                    <form action="/action_administrator.php">
+                        <h4>Ajout Langue</h4>
+                        <input type="text" name="language" placeholder="Enter a language" required>
+                        <button type="submit">Ajout</button>
+                    </form>
+                    <!-- ============================================== -->
+                </div>
+            </div>
+        </header>
+    </body>
+    <script src="./js/jquery-1.12.3.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+</html>
