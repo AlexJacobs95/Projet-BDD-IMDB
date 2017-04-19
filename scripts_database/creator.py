2cf1feac942ff389dab84af90063cb884f5a4c20
David <David.Engelman@ulb.ac.be>
2017-04-19 12:48:01 +0200

    creator.py

--- scripts_database/creator.py
+++ scripts_database/creator.py
@@ -1,6 +1,8 @@
 
-import mysql.connector
-import os
+import MySQLdb as mdb
+import datetime
+
+talbes = ["Personne", "Oeuvre", "Pays", "Genre", "Langue", "Film", "Serie", "Auteur", "Directeur", "Episode", "Acteur"]
 
 def executeScriptsFromFile(filename, cursor):
     # Open and read the file as a single buffer
@@ -11,20 +13,34 @@ def executeScriptsFromFile(filename, cursor):
     sqlCommands = sqlFile.split(';')
 
     # Execute every command from the input file
+    i = 0
+
+    global_time = datetime.datetime.now()
+    print ("Insertion started.")
     for command in sqlCommands:
+        table_time = datetime.datetime.now()
+        print ("Filling table "  + talbes[i] + " ...")
         try:
             cursor.execute(command)
-        except OperationalError, msg:
-            print "Command skipped: ", msg
+
+        except mdb.Error, e:
+            pass
+        print ("table " + talbes[i] + "filled in " + str(datetime.datetime.now() - table_time) + "s" )
+
+        i+=1
+
+    print ("Insertion done\nDatabase filled in " + str(datetime.datetime.now() - global_time) + "s")
+    size = cursor.execute("SELECT table_schema \"IMBD\", Round(Sum(data_length + index_length) / 1024 / 1024, 1) \"DB Size in MB\" FROM   information_schema.tables GROUP  BY table_schema; ")
+    print ("The DB size is " + str(size))
 
 
 
-conn = mysql.connector(host="localhost",user="root",password="XXX", database="")
+conn = mdb.connect("localhost","root","","")
 cur = conn.cursor()
-cur.execute('CREATE DATABASE IMBD;')
+cur.execute("CREATE DATABASE IMBD;")
 conn.close();
 
-conn = mysql.connector("localhost","root","XXX","IMBD")
+conn = mdb.connect("localhost","root","","IMBD")
 cur = conn.cursor()
 
 executeScriptsFromFile("ddl.sql", cur)
