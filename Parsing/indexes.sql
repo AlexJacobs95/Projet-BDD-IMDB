512b46c87a4bfe80c0664476b208dc6bb1a2fab6
David <David.Engelman@ulb.ac.be>
2017-04-29 14:56:57 +0200

    indexes

---
+++ Parsing/indexes.sql
@@ -0,0 +1,3 @@
+\! echo "Indexing...\n";
+CREATE index TitreOeuvre on Oeuvre(Titre);
+CREATE index NomPersonne on Personne(Nom);
--- scripts_database/fill.sh
+++ scripts_database/fill.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 python creator.py
 mysql -u root -p IMDB < load_data.sql
+mysql -u root -p IMDB < indexes.sql
