42ce19e69685e3646778465b18d3f3517b1de0f8
Alexandre Jacobs <alejacob@ulb.ac.be>
2017-05-05 15:33:29 +0200

    add function for movie trailers

--- site_web/API.js
+++ site_web/API.js
@@ -2,7 +2,7 @@ function getImagesMovie(title, date) {
     theMovieDb.search.getMovie({"query": encodeURI(title), "year": date}, function (data) {
         data = JSON.parse(data); // parse the data
         const data_ok = data['results'];
-        console.log(data)
+        console.log(data);
         const posterPath = "https://image.tmdb.org/t/p/w500" + data_ok[0]["poster_path"];
         const backDropPath = "https://image.tmdb.org/t/p/w1000" + data_ok[0]["backdrop_path"];
 
@@ -15,6 +15,19 @@ function getImagesMovie(title, date) {
             $("header").css("background-image", path);
         }
 
+    }, function (error) {
+    })
+}
+
+function getTrailersMovie(title, date) {
+    theMovieDb.search.getTrailers({"query": encodeURI(title), "year": date}, function(data) {
+        data = JSON.parse(data); //parse the data
+        const data_ok = data["results"];
+        console.log(data);
+        const videoPath = "https://www.youtube.com/embed/"+ data_ok["key"]+"?controls=1";
+        if (data_ok[0]["key"] != null) {
+            //mettre dans la page
+        }
     }, function (error) {
     })
 }
