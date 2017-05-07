afd2b1b6f8fa4e8e894d6faa709d764b3c9e241c
David <David.Engelman@ulb.ac.be>
2017-05-07 14:09:52 +0200

    queries recherche ok

--- scripts_database/indexes.sql
+++ scripts_database/indexes.sql
@@ -1,3 +1,5 @@
 \! echo "Indexing...\n";
 CREATE fulltext index titreIndex on Oeuvre(Titre);
 CREATE fulltext index nomIndex on Personne(Prenom,Nom);
+CREATE index ontitre On Oeuvre(Titre);
+CREATE index fullnameIndex On Personne(fullname);
